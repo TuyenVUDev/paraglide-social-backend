7ef8eea71c3995f176b63894aebe31f00b7ec443
Tuyen <tuyentown@gmail.com>
2023-07-15 12:31:49 +0200

    maven init

---
+++ src/main/java/com/tuyenVUDev/paraglidesocial/ParaglideSocialApplication.java
@@ -0,0 +1,13 @@
+package com.tuyenVUDev.paraglidesocial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParaglideSocialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParaglideSocialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuyenVUDev/paraglidesocial/ParaglideSocialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuyenVUDev.paraglidesocial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParaglideSocialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
